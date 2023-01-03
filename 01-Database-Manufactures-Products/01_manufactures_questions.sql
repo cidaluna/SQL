fa7121ae1785fc20203033d7c994574e794341fc
cidaluna <cidalunna@hotmail.com>
2023-01-02 21:02:49 -0300

    Some SQL exercises

--- 01-Database-Manufactures-Products/01_manufactures_questions.sql
+++ 01-Database-Manufactures-Products/01_manufactures_questions.sql
@@ -3,3 +3,7 @@
 -- 2. Select the names and the prices of all the products in the store.
 -- 3. Select the name of the products with a price less than or equal to $200. 
 -- 4. Select all the products with a price between $60 and $120. 
+-- 5. Select the name and price in cents (i.e., the price must be multiplied by 100). 
+-- 6. Compute the average price of all the products. 
+-- 7. Compute the average price of all products with manufacturer code equal to 2. 
+-- 8. Compute the number of products with a price larger than or equal to $180. 
--- 01-Database-Manufactures-Products/01_questions_and_solution.sql
+++ 01-Database-Manufactures-Products/01_questions_and_solution.sql
@@ -20,10 +20,32 @@ WHERE
     PRICE <= 200
 ;
 
---4. Select all the products with a price between $60 and $120. 
+-- 4. Select all the products with a price between $60 and $120. 
 SELECT *
 FROM
     PRODUCTS
 WHERE
     PRICE BETWEEN 60 AND 120
 ;
+
+-- 5. Select the name and price in cents (i.e., the price must be multiplied by 100). 
+SELECT NAME, (PRICE * 100) PRICE_IN_CENTS
+FROM
+    PRODUCTS
+;
+
+-- 6. Compute the average price of all the products. 
+SELECT AVG(PRICE)
+FROM
+    PRODUCTS
+;
+
+-- 7. Compute the average price of all products with manufacturer code equal to 2. 
+SELECT AVG(PRICE)
+FROM
+    PRODUCTS
+WHERE
+    MANUFACTURER = 2
+;
+
+-- 8. Compute the number of products with a price larger than or equal to $180. 
