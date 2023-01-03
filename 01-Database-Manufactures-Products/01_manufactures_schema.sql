0174046010fd11752377f84e8e53c8c7acdb902a
cidaluna <cidalunna@hotmail.com>
2023-01-03 19:36:36 -0300

    Inserted some data

--- 01-Database-Manufactures-Products/01_manufactures_schema.sql
+++ 01-Database-Manufactures-Products/01_manufactures_schema.sql
@@ -15,3 +15,9 @@ CREATE TABLE PRODUCTS(
     CONSTRAINT PRODUCTS_PK PRIMARY KEY (PCODE),
     CONSTRAINT FK_MANUFACTURERS_CODE REFERENCES MANUFACTURERS(MCODE)
 );
+
+
+-- SAMPLE DATASET
+INSERT INTO MANUFACTURERS(MCODE,NAME) VALUES (1,'Sony'),(2,'Creative Labs'),(3,'Hewlett-Packard'),(4,'Iomega'),(5,'Fujitsu'),(6,'Winchester');
+INSERT INTO PRODUCTS(PCODE,NAME,PRICE,MANUFACTURER) VALUES(1,'Hard drive',240,5),(2,'Memory',120,6),(3,'ZIP drive',150,4),(4,'Floppy disk',5,6),(5,'Monitor',240,1),
+(6,'DVD drive',180,2),(7,'CD drive',90,2),(8,'Printer',270,3),(9,'Toner cartridge',66,3),(10,'DVD burner',180,2);
