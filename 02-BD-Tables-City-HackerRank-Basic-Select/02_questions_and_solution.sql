c101f6d47b1c7171ccd9a3ff39987e67128ebd5e
cidaluna <cidalunna@hotmail.com>
2023-01-31 22:08:51 -0300

    Sql exercises

--- 02-BD-Tables-City-HackerRank-Basic-Select/02_questions_and_solution.sql
+++ 02-BD-Tables-City-HackerRank-Basic-Select/02_questions_and_solution.sql
@@ -96,5 +96,18 @@ AND CITY NOT LIKE '%a' AND CITY NOT LIKE '%e' AND CITY NOT LIKE '%i' AND CITY NO
 
 -- 19. Query the Name of any student in STUDENTS who scored higher than Marks. Order your output by the last three characters of each name. 
 -- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
-SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME, 3), ID;
+SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY SUBSTR(NAME, -3) ASC, ID ASC;
 
+-- 20. Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC;
+
+-- 21. Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a 
+-- salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID ASC;
+
+-- 22. Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
+-- Output one of the following statements for each record in the table:
+--    Equilateral: It's a triangle with 3 sides of equal length.
+--    Isosceles: It's a triangle with 2 sides of equal length.
+--    Scalene: It's a triangle with 3 sides of differing lengths.
+--    Not A Triangle: The given values of A, B, and C don't form a triangle.
